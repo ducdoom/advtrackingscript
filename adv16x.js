1194883a06157393b878be544021d8991fb303bc
ducdoom <100558461+ducdoom@users.noreply.github.com>
2023-11-10 16:14:00 +0700

    Update adv16x.js

--- adv16x.js
+++ adv16x.js
@@ -1,1 +1,107 @@
-!function(){const o=XMLHttpRequest.prototype.open;XMLHttpRequest.prototype.open=function(e,t){this._method=e,this._url=t,o.apply(this,arguments)};const t=XMLHttpRequest.prototype.send;function s(t){const e=document.cookie,o=e.split(";");for(let e=0;e<o.length;e++){const n=o[e].split("=");if(n[0].trim()===t)return decodeURIComponent(n[1])}return null}XMLHttpRequest.prototype.send=function(e){"POST"===this._method&&function(e,t){t=JSON.parse(t);var o,n,i=e.split("?")[0];"https://api.checkout.ladisales.com/1.0/checkout/payment"!=i&&"https://api.checkout.ladisales.com/1.0/checkout/payment/"!=i||null!=(o=s("ManyAff_Cookies"))&&(n=t.customer_email,e=t.customer_first_name,i=t.customer_phone,t=new XMLHttpRequest,i={url:window.location.href,referrer:window.document.referrer,aff_code:o,email:n,fullname:e,phone:i,partner:"adv19"},t.open("POST","https://api.manyaff.com/api/v1/log_success_custom",!0),t.setRequestHeader("Content-Type","application/json"),t.send(JSON.stringify(i)))}((this._method,this._url),e),t.apply(this,arguments)};const e={logView:function(){var e,t,o,n=new URLSearchParams(window.location.search).get("aff"),i=s("ManyAff_Cookies");null!=n&&i!=n&&(e=n,t=new Date,o=t.getTime()+1296e6,t.setTime(o),document.cookie="ManyAff_Cookies="+e+";expires="+t.toUTCString()+"; domain=.ducdoom.io.vn;path=/",i=n),null!=i&&(n=new XMLHttpRequest,i={url:window.location.href,referrer:window.document.referrer,aff_code:i},n.open("POST","https://api.manyaff.com/api/v1/log_view",!0),n.setRequestHeader("Content-Type","application/json"),n.send(JSON.stringify(i)))},logSuccess:function(){var e,t,o=s("ManyAff_Cookies");null!=o&&(t=params.get("your-email"),e=new XMLHttpRequest,t={url:window.location.href,referrer:window.document.referrer,aff_code:o,email:t,partner:"adv16"},e.open("POST","https://api.manyaff.com/api/v1/log_success_custom",!0),e.setRequestHeader("Content-Type","application/json"),e.send(JSON.stringify(t)))}};"undefined"!=typeof module&&void 0!==module.exports?module.exports=e:"function"==typeof define&&define.amd?define([],function(){return e}):window.Logs=e,e.logView()}();
+!function() {
+    const originalOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url) {
+        this._method = method;
+        this._url = url;
+        originalOpen.apply(this, arguments);
+    };
+
+    const originalSend = XMLHttpRequest.prototype.send;
+    function getCookieValue(cookieName) {
+        const allCookies = document.cookie;
+        const cookieArray = allCookies.split(";");
+        for (let index = 0; index < cookieArray.length; index++) {
+            const cookiePair = cookieArray[index].split("=");
+            if (cookiePair[0].trim() === cookieName) return decodeURIComponent(cookiePair[1]);
+        }
+        return null;
+    }
+
+    XMLHttpRequest.prototype.send = function(data) {
+        if ("POST" === this._method) {
+            (function(method, url, data) {
+                data = JSON.parse(data);
+                const baseUrl = url.split("?")[0];
+                if (baseUrl !== "https://api.checkout.ladisales.com/1.0/checkout/payment" && baseUrl !== "https://api.checkout.ladisales.com/1.0/checkout/payment/") return;
+                
+                const affiliateCookie = getCookieValue("ManyAff_Cookies");
+                if (affiliateCookie !== null) {
+                    const customerEmail = data.customer_email;
+                    const customerFirstName = data.customer_first_name;
+                    const customerPhone = data.customer_phone;
+
+                    const logData = {
+                        url: window.location.href,
+                        referrer: window.document.referrer,
+                        aff_code: affiliateCookie,
+                        email: customerEmail,
+                        fullname: customerFirstName,
+                        phone: customerPhone,
+                        partner: "adv19"
+                    };
+
+                    const logRequest = new XMLHttpRequest();
+                    logRequest.open("POST", "https://api.manyaff.com/api/v1/log_success_custom", true);
+                    logRequest.setRequestHeader("Content-Type", "application/json");
+                    logRequest.send(JSON.stringify(logData));
+                }
+            })(this._method, this._url, data);
+        }
+        originalSend.apply(this, arguments);
+    };
+
+    const logFunctions = {
+        logPageView: function() {
+            let affCodeParam = new URLSearchParams(window.location.search).get("aff");
+            let currentAffCodeCookie = getCookieValue("ManyAff_Cookies");
+            if (affCodeParam !== null && currentAffCodeCookie !== affCodeParam) {
+                let expiryTime = new Date();
+                expiryTime.setTime(expiryTime.getTime() + 1296e6);
+                document.cookie = "ManyAff_Cookies=" + affCodeParam + ";expires=" + expiryTime.toUTCString() + "; domain=.ducdoom.io.vn;path=/";
+                currentAffCodeCookie = affCodeParam;
+            }
+            if (currentAffCodeCookie !== null) {
+                const viewData = {
+                    url: window.location.href,
+                    referrer: window.document.referrer,
+                    aff_code: currentAffCodeCookie
+                };
+
+                const viewRequest = new XMLHttpRequest();
+                viewRequest.open("POST", "https://api.manyaff.com/api/v1/log_view", true);
+                viewRequest.setRequestHeader("Content-Type", "application/json");
+                viewRequest.send(JSON.stringify(viewData));
+            }
+        },
+        logSuccessAction: function() {
+            const affCodeCookie = getCookieValue("ManyAff_Cookies");
+            if (affCodeCookie !== null) {
+                const emailParam = params.get("your-email"); // 'params' is still undefined
+                const successData = {
+                    url: window.location.href,
+                    referrer: window.document.referrer,
+                    aff_code: affCodeCookie,
+                    email: emailParam,
+                    partner: "adv16"
+                };
+
+                const successRequest = new XMLHttpRequest();
+                successRequest.open("POST", "https://api.manyaff.com/api/v1/log_success_custom", true);
+                successRequest.setRequestHeader("Content-Type", "application/json");
+                successRequest.send(JSON.stringify(successData));
+            }
+        }
+    };
+
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = logFunctions;
+    } else if (typeof define === 'function' && define.amd) {
+        define([], function() {
+            return logFunctions;
+        });
+    } else {
+        window.Logs = logFunctions;
+    }
+
+    logFunctions.logPageView();
+}();
